94dc774733a142fc9402f79f410e7181d411ed90
Jessy Damasceno <jessy_bas@hotmail.com>
2022-06-22 16:41:42 -0300

    add login styles

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,12 +1,31 @@
 import React from 'react';
 import '../styles/Login.css';
-import TrybeLogo from '../components/TrybeLogo';
 
 class Login extends React.Component {
   render() {
     return (
       <section className="login__page">
-        <TrybeLogo />
+        <form>
+          <span className="text-center">login</span>
+          <div className="input-container">
+            <input
+              placeholder="e-mail"
+              data-testid="email-input"
+              type="email"
+              name="email"
+              id="email"
+            />
+          </div>
+          <div className="input-container">
+            <input
+              placeholder="senha"
+              data-testid="password-input"
+              type="password"
+              name="email"
+            />
+          </div>
+          <button type="button" className="btn">Entrar</button>
+        </form>
       </section>
     );
   }
