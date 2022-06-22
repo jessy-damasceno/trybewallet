841ce9324f9bb75098985ba5cad415501060a842
Jessy Damasceno <jessy_bas@hotmail.com>
2022-06-22 17:23:58 -0300

    add state, handleChange to Login page

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -2,7 +2,35 @@ import React from 'react';
 import '../styles/Login.css';
 
 class Login extends React.Component {
+  state = {
+    email: '',
+    password: '',
+    isDisabled: false,
+  }
+
+  handleChange = ({ target }) => {
+    const { name, value } = target;
+
+    this.setState({ [name]: value });
+  }
+
+  handleClick = () => {
+    const { email, password } = this.state;
+
+    console.log(email.match(/\w+@[a-z]+.com/g));
+    console.log(password);
+  }
+
   render() {
+    const { email, password } = this.state;
+    const regExp = /\w+@[a-z]+.com/g;
+    let { isDisabled } = this.state;
+    const magicNumber = 6;
+
+    if (email.match(regExp) && password.length >= magicNumber) {
+      isDisabled = true;
+    }
+
     return (
       <section className="login__page">
         <form>
@@ -14,6 +42,8 @@ class Login extends React.Component {
               type="email"
               name="email"
               id="email"
+              onChange={ this.handleChange }
+              value={ email }
             />
           </div>
           <div className="input-container">
@@ -21,10 +51,20 @@ class Login extends React.Component {
               placeholder="senha"
               data-testid="password-input"
               type="password"
-              name="email"
+              name="password"
+              id="password"
+              onChange={ this.handleChange }
+              value={ password }
             />
           </div>
-          <button type="button" className="btn">Entrar</button>
+          <button
+            type="button"
+            className={ !isDisabled ? 'btn disabled' : 'btn' }
+            onClick={ this.handleClick }
+            disabled={ !isDisabled }
+          >
+            Entrar
+          </button>
         </form>
       </section>
     );
